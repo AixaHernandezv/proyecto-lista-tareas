2775770db431d2c4d917d0892a1365f2a95718cc
AixaHernandezv <aixahernandezv@gmail.com>
2023-10-01 18:02:40 -0300

    cambiar estilos y eliminar escuchador del check

--- js/main.js
+++ js/main.js
@@ -1,58 +1,55 @@
-const taskInput = document.querySelector('#taskInput');
-const btnAddTask = document.querySelector('#btnAddTask');
-const taskContainer = document.querySelector('#taskContainer');
-let tasks =[];
+const taskInput = document.querySelector("#taskInput");
+const btnAddTask = document.querySelector("#btnAddTask");
+const taskContainer = document.querySelector("#taskContainer");
+let tasks = [];
 
-btnAddTask.addEventListener('click', addTask);
-taskInput.addEventListener('keyup', (evt) => {
-    if (evt.code === 'Enter') {
+btnAddTask.addEventListener("click", addTask);
+taskInput.addEventListener("keyup", (evt) => {
+    if (evt.code === "Enter") {
         addTask();
     }
-})
-
+});
 
 function addTask() {
     if (taskInput.value === "") {
         alert("no hay nada que agegar");
         taskInput.focus();
-        return
-    } 
-    
+        return;
+    }
+
     tasks.push(taskInput.value);
     console.log(tasks);
     listTasks();
     taskInput.value = "";
-    taskInput.focus(); 
-
-    
+    taskInput.focus();
 }
 
-
 function listTasks() {
-    taskContainer.innerHTML='';
-    tasks.forEach((task, index)=> {
-        const divTaskHtml = document.createElement('div');
-        divTaskHtml.classList.add('tasks__item');
+    taskContainer.innerHTML = "";
+    tasks.forEach((task, index) => {
+        const divTaskHtml = document.createElement("div");
+        divTaskHtml.classList.add("tasks__item");
         divTaskHtml.innerHTML = `
             <div id="task" class=" item__task">
                 <p id="taskParagraph${index}" class="task__paragraph"> ${task} </p>
             </div>
             <i  id="btnCheck${index}"  class="fa-solid fa-check item__btn" data-id="${index}"></i>
             <i class="fa-solid fa-xmark item__btn"></i>
-        `
+        `;
         taskContainer.appendChild(divTaskHtml);
         const btnCheck = document.querySelector(`#btnCheck${index}`);
-        
-        btnCheck.addEventListener('click',addClassCrossedOut);
+        btnCheck.addEventListener("click", addClassCrossedOut);
     });
-    
 }
-
 
 function addClassCrossedOut(e) {
-const index = e.target.getAttribute("data-id");
-const taskParagraph = document.querySelector(`#taskParagraph${index}`)
-taskParagraph.classList.add('task__crossedout');
+    console.log(e.target);
+    const index = e.target.getAttribute("data-id");
+    const taskParagraph = document.querySelector(`#taskParagraph${index}`);
+    taskParagraph.classList.add("task__crossedout");
+    const btnCheck = document.querySelector(`#btnCheck${index}`);
+    btnCheck.classList.remove('item__btn');
+    btnCheck.classList.add('item__btn--disabled');
+    btnCheck.removeEventListener("click", addClassCrossedOut);
 
-
-}
+};
